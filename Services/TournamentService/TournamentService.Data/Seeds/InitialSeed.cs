298e472729134e5e865db9130a81e900b04068dc
blindlobstar <boryapictures@gmail.com>
2021-01-11 21:24:40 +0300

    add strategi for initial seed

--- Services/TournamentService/TournamentService.Data/Seeds/InitialSeed.cs
+++ Services/TournamentService/TournamentService.Data/Seeds/InitialSeed.cs
@@ -108,96 +108,101 @@ namespace TournamentService.Data.Seeds
         public static void EnsureSeedIdentityInsert(this TournamentContext dataContext)
         {
             dataContext.Database.EnsureCreated();
-            using var transaction = dataContext.Database.BeginTransaction();
-            var tournament = new TournamentDto()
+            var strategy = dataContext.Database.CreateExecutionStrategy();
+            strategy.Execute(() =>
             {
-                Id = 1,
-                Caption = "New Tournament",
-                Description = "First added tournament",
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now.AddDays(3),
-                TournamentTime = 20
-            };
-            var tournament2 = new TournamentDto()
-            {
-                Id = 2,
-                Caption = "Old Tournament",
-                Description = "First added tournament",
-                StartDate = DateTime.Now,
-                EndDate = DateTime.Now.AddDays(3),
-                TournamentTime = 20
-            };
-
-            if (!dataContext.Tournaments.Any())
-            {
-                dataContext.Tournaments.Add(tournament);
-                dataContext.Tournaments.Add(tournament2);
-                dataContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Tournament ON");
-                dataContext.SaveChanges();
-                dataContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Tournament OFF");
-            }
-
-            if (!dataContext.TournamentsUsers.Any())
-            {
-                dataContext.TournamentsUsers.Add(new TournamentsUsers()
+                using var transaction = dataContext.Database.BeginTransaction();
+                var tournament = new TournamentDto()
                 {
-                    Tournament = tournament,
-                    UserId = "5e7398bde6ab1940182c5cfd"
-                });
-                dataContext.SaveChanges();
-            }
+                    Id = 1,
+                    Caption = "New Tournament",
+                    Description = "First added tournament",
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now.AddDays(3),
+                    TournamentTime = 20
+                };
+                var tournament2 = new TournamentDto()
+                {
+                    Id = 2,
+                    Caption = "Old Tournament",
+                    Description = "First added tournament",
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now.AddDays(3),
+                    TournamentTime = 20
+                };
 
-            if (!dataContext.Exercises.Any())
-            {
-                dataContext.Exercises.AddRange(
-                    new ExerciseDto()
-                    {
-                        Id = 1,
-                        Text = "1+1",
-                        Answer = "2",
-                        OrderNumber = 1,
-                        Tournament = tournament
-                    }, new ExerciseDto()
-                    {
-                        Id = 2,
-                        Text = "3+5",
-                        Answer = "8",
-                        OrderNumber = 2,
-                        Tournament = tournament
-                    }, new ExerciseDto()
-                    {
-                        Id = 3,
-                        Text = "First three letters of alphabet",
-                        Answer = "a,b,c",
-                        OrderNumber = 3,
-                        Tournament = tournament
-                    }, new ExerciseDto()
-                    {
-                        Id = 4,
-                        Text = "First three letters of alphabet",
-                        Answer = "a,b,c",
-                        OrderNumber = 3,
-                        Tournament = tournament2
-                    });
-                dataContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Exercise ON");
-                dataContext.SaveChanges();
-                dataContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Exercise OFF");
-            }
+                if (!dataContext.Tournaments.Any())
+                {
+                    dataContext.Tournaments.Add(tournament);
+                    dataContext.Tournaments.Add(tournament2);
+                    dataContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Tournament ON");
+                    dataContext.SaveChanges();
+                    dataContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Tournament OFF");
+                }
 
-            if (!dataContext.ExercisesUsers.Any())
-            {
-                dataContext.ExercisesUsers.AddRange(
-                    new ExercisesUsers()
+                if (!dataContext.TournamentsUsers.Any())
+                {
+                    dataContext.TournamentsUsers.Add(new TournamentsUsers()
                     {
-                        ExerciseId = 1,
-                        UserId = "5e7398bde6ab1940182c5cfd",
-                        UserAnswer = "2",
-                        IsCorrect = true
+                        Tournament = tournament,
+                        UserId = "5e7398bde6ab1940182c5cfd"
                     });
+                    dataContext.SaveChanges();
+                }
+
+                if (!dataContext.Exercises.Any())
+                {
+                    dataContext.Exercises.AddRange(
+                        new ExerciseDto()
+                        {
+                            Id = 1,
+                            Text = "1+1",
+                            Answer = "2",
+                            OrderNumber = 1,
+                            Tournament = tournament
+                        }, new ExerciseDto()
+                        {
+                            Id = 2,
+                            Text = "3+5",
+                            Answer = "8",
+                            OrderNumber = 2,
+                            Tournament = tournament
+                        }, new ExerciseDto()
+                        {
+                            Id = 3,
+                            Text = "First three letters of alphabet",
+                            Answer = "a,b,c",
+                            OrderNumber = 3,
+                            Tournament = tournament
+                        }, new ExerciseDto()
+                        {
+                            Id = 4,
+                            Text = "First three letters of alphabet",
+                            Answer = "a,b,c",
+                            OrderNumber = 3,
+                            Tournament = tournament2
+                        });
+                    dataContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Exercise ON");
+                    dataContext.SaveChanges();
+                    dataContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Exercise OFF");
+                }
+
+                if (!dataContext.ExercisesUsers.Any())
+                {
+                    dataContext.ExercisesUsers.AddRange(
+                        new ExercisesUsers()
+                        {
+                            ExerciseId = 1,
+                            UserId = "5e7398bde6ab1940182c5cfd",
+                            UserAnswer = "2",
+                            IsCorrect = true
+                        });
+                    dataContext.SaveChanges();
+                }
+
                 dataContext.SaveChanges();
-            }
-            dataContext.SaveChanges();
-            transaction.Commit();
+                transaction.Commit();
+            });
         }
     }
 }
