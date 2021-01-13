b1886d41713f85fa4e1c2f2d795f704461ba6c65
blindlobstar <boryapictures@gmail.com>
2021-01-13 14:09:41 +0300

    Add new project for ExerciseFlow service

---
+++ Services/ExerciseFlow/ExerciseFlow.API/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseFlow.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseEndpoints(endpoints =>
+            {
+
+            });
+        }
+    }
+}
